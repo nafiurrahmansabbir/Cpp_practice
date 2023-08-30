da0932416b3b446edd2c3be61b825a2cc6c4ba1e
Nafiur Rahman Sabbir <nafiurrahmansabbir@gmail.com>
2023-08-30 22:19:35 +0600

    first  commit

---
+++ folder/input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amount1,amount2,sum;
+    cout<<"Enter 1st amount : ";
+    cin>>amount1;
+    cout<<"Enter 2nd amount: ";
+    cin>>amount2;
+    sum=amount1+amount2;
+    cout<<"The total sum of amount is: "<<sum<<"\n";
+}
